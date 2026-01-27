1c97092229fa284276b1136d87372d7abcd30485
rowan-con <maeroann07@gmail.com>
2026-01-28 00:09:52 +0800

    Update main menu scripts and scene

--- main menu bg/main_menu.gd
+++ main menu bg/main_menu.gd
@@ -31,3 +31,9 @@ func _on_button_2_pressed() -> void:
 
 func _on_back_options_pressed() -> void:
 	_ready()
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://scene/levelroot1.tscn")
+
+	
