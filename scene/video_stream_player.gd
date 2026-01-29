7ca49af4b84ccfb1ba224aaf57118493aab601f1
CARLFHG <gcarlseigfred@gmail.com>
2026-01-29 08:26:35 +0800

    chest

--- scene/video_stream_player.gd
+++ scene/video_stream_player.gd
@@ -1,23 +1,39 @@
 extends VideoStreamPlayer
 
-# Path must go UP to levelroot3, then into Area2D, then to Button
-@onready var pause_btn = get_node("../Area2D/Button") 
+# Correct paths: Up to levelroot3, then into Area2D or TileMapLayer
+@onready var pause_btn = get_node("../Area2D/Button")
+@onready var tilemap = get_node("../TileMapLayer")
+
+var is_first_pause = true
 
 func _ready():
+	# Connect signal to loop the video
 	finished.connect(_on_finished)
-	pause_btn.hide() # This will now work because the path is correct
+	# Hide button until player enters the trigger area
+	pause_btn.hide()
 
 func _on_finished():
 	play()
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.name == "player": 
-		pause_btn.show()
-
-func _on_area_2d_body_exited(body: Node2D) -> void:
-	if body.name == "player":
-		pause_btn.hide()
+# This function runs when you click the 'pause' button
 
 func _on_button_pressed() -> void:
 	paused = !paused
 	pause_btn.text = "Play" if paused else "Pause"
+	
+	var chest_pos = Vector2i(62, 20)
+	
+	if paused:
+		# Open Chest: Source 4, Atlas (0, 0)
+		tilemap.set_cell(chest_pos, 4, Vector2i(0, 0))
+	else:
+		# Closed Chest: Source 3, Atlas (0, 0) <--- FIXED SOURCE ID
+		tilemap.set_cell(chest_pos, 3, Vector2i(0, 0))
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.name == "player":
+		pause_btn.show() # This makes it visible
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.name == "player":
+		pause_btn.hide() # This hides it when you walk away
