83f588767c4d9a4494fe05363775e628984c1424
WLA232 <jebronagulto04@gmail.com>
2026-01-30 22:37:25 +0800

    following inve

--- scene/TEMP.gd
+++ scene/TEMP.gd
@@ -3,6 +3,9 @@ extends Area2D
 @export var drag_scale := 1.2
 @export var damage := 10
 @export var damage_cooldown := 0.2
+@onready var icon_sprite: Sprite2D = $Sprite2D
+@onready var video: VideoStreamPlayer = $VideoStreamPlayer
+
 
 var original_position: Vector2
 var original_scale: Vector2
@@ -39,8 +42,6 @@ func start_drag():
 	scale = original_scale * drag_scale
 	z_index = 100
 	damage_timer = 0.0
-
-
 
 func end_drag():
 	dragging = false
